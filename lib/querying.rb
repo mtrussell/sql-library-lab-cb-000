96ef86e6e050345f0e4cad89f0867504fbb4e1ed
Matt Trussell <smtrussell@gmail.com>
2017-11-21 02:39:56 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,6 +47,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters
   ON series.id = characters.series_id
   WHERE characters.species = 'human'
+  GROUP ON series.title
   "
 end
 
